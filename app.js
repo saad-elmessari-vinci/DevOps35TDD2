b2ce098d168f169e7099d88708aeeecc4d9f08db
Haroune Jamili <haroune.jamili@student.vinci.be>
2024-12-10 10:41:06 +0100

    create: new routes

--- app.js
+++ app.js
@@ -41,6 +41,8 @@ const exoluneRouter = require("./routes/exolunes.js");
 const usersRouter = require('./routes/users.js');
 const membersRouter = require('./routes/members.js');
 const adminRouter = require('./routes/admin.js');
+const planetsRouter=require('./routes/planets.js');
+const proposalsRouter=require('./routes/proposals.js');
 
 const app = express();
 const port = 3000;
@@ -69,6 +71,8 @@ app.use("/exolunes", exoluneRouter);
 app.use("/users", usersRouter);
 app.use("/members", membersRouter);
 app.use("/admin", adminRouter);
+app.use("/planets",planetsRouter);
+app.use("/proposals",proposalsRouter);
 
 
 // Create error on page not found
